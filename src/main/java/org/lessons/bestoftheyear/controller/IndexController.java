0a7835676550e1ee6e3b338f92ab011703c6ed68
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-08 09:20:45 +0100

    Fix route home-page

--- src/main/java/org/lessons/bestoftheyear/controller/IndexController.java
+++ src/main/java/org/lessons/bestoftheyear/controller/IndexController.java
@@ -1,18 +1,27 @@
-package org.lessons.bestoftheyear.model;
+package org.lessons.bestoftheyear.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping
+    @ResponseBody
+    public String index() {
+        return "<html><body><h1>I'm Index</h1></body></html>";
+    }
+
+    @GetMapping("home")
     public String home(Model model) {
         model.addAttribute("name", "Antonio");
-        return "index";
+        return "home-page";
     }
 
     private List<String> getBestMovies() {
@@ -23,7 +32,7 @@ public class IndexController {
         return List.of("Money", "The Wall", "The Dark Side of the Moon");
     }
 
-    @GetMapping("/movies")
+    @GetMapping("movies")
     public String bestMovies(Model model) {
         List<String> movies = getBestMovies();
         String movieList = String.join(", ", movies);
@@ -31,7 +40,7 @@ public class IndexController {
         return "movies";
     }
 
-    @GetMapping("/songs")
+    @GetMapping("songs")
     public String bestSongs(Model model) {
         List<String> songs = getBestSongs();
         String songList = String.join(", ", songs);
