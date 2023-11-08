96cfeb18e11fc21d5a5b18df18a6b92e6b51c230
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-08 17:19:32 +0100

    Add Step 2

--- src/main/java/org/lessons/bestoftheyear/controller/MovieController.java
+++ src/main/java/org/lessons/bestoftheyear/controller/MovieController.java
@@ -4,6 +4,7 @@ import org.lessons.bestoftheyear.model.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -20,6 +21,13 @@ public class MovieController {
         return "movie-list";
     }
 
+    @GetMapping("/{id}")
+    public String movieDetail(@PathVariable("id") int movieId, Model model) {
+        Movie movie = getMovieById(movieId);
+        model.addAttribute("movie", movie);
+        return "movie-detail";
+    }
+
     private List<Movie> getBestMovies() {
         Movie[] bestMovies = {
             new Movie(1,"Matrix"),
@@ -27,5 +35,14 @@ public class MovieController {
             new Movie(3,"Terminator")
         };
         return Arrays.asList(bestMovies);
+    }
+
+    private Movie getMovieById(int id) {
+        for (Movie movie : getBestMovies()) {
+            if (movie.getId() == (id)) {
+                return movie;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/org/lessons/bestoftheyear/controller/SongController.java
+++ src/main/java/org/lessons/bestoftheyear/controller/SongController.java
@@ -1,9 +1,11 @@
 package org.lessons.bestoftheyear.controller;
 
+import org.lessons.bestoftheyear.model.Movie;
 import org.lessons.bestoftheyear.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -20,6 +22,13 @@ public class SongController {
         return "song-list";
     }
 
+    @GetMapping("/{id}")
+    public String songDetail(@PathVariable("id") int songId, Model model) {
+        Song song = getSongById(songId);
+        model.addAttribute("song", song);
+        return "song-detail";
+    }
+
     private List<Song> getBestSongs() {
         Song[] bestSongs = {
                 new Song(1,"Money"),
@@ -27,5 +36,14 @@ public class SongController {
                 new Song(3,"The Dark Side of the Moon")
         };
         return Arrays.asList(bestSongs);
+    }
+
+    private Song getSongById(int id) {
+        for (Song song : getBestSongs()) {
+            if (song.getId() == (id)) {
+                return song;
+            }
+        }
+        return null;
     }
 }
